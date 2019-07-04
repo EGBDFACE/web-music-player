aceba4f8c7e1b659d0943efc767c12dfbf481581
JackChuChou <3212497319@qq.com>
2019-07-04 16:58:25 +0800

    add: deploy

--- config/index.js
+++ config/index.js
@@ -52,7 +52,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    // assetsPublicPath: '/',
+    assetsPublicPath: '/web-music-player/',
 
     /**
      * Source Maps
